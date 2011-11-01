f45cf2e2269fb48c7658ccc35d678beab871f126
CrBoy <crboy@crboy.net>
2011-11-02 07:28:00 +0800

    Add gdb settings

--- install.sh
+++ install.sh
@@ -6,5 +6,7 @@ ln -si $PWD/zshrc           $INSTALL_DIR/.zshrc
 ln -si $PWD/ctags           $INSTALL_DIR/.ctags
 ln -si $PWD/gitconfig       $INSTALL_DIR/.gitconfig
 ln -si $PWD/gitignore       $INSTALL_DIR/.gitignore
+ln -si $PWD/gdb             $INSTALL_DIR/.gdb
+ln -si $PWD/gdb/gdbinit     $INSTALL_DIR/.gdbinit
 
 
