03331a5ebc426ce9d43fa0ff8c163953b7fc315d
CrBoy <crboy@crboy.net>
2012-06-16 01:07:32 +0800

    Revise settings of gitignore (exclude files)

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ INSTALL_DIR=~
 ln -si $PWD/zshrc           $INSTALL_DIR/.zshrc
 ln -si $PWD/ctags           $INSTALL_DIR/.ctags
 ln -si $PWD/gitconfig       $INSTALL_DIR/.gitconfig
-ln -si $PWD/gitignore       $INSTALL_DIR/.gitignore
 ln -si $PWD/gdb             $INSTALL_DIR/.gdb
 ln -si $PWD/gdb/gdbinit     $INSTALL_DIR/.gdbinit
 
