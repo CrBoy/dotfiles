b52827d65496e1577b78299827cda1cd037d88b1
CrBoy <crboy@crboy.net>
2012-06-20 22:37:53 +0800

    Add tmux.conf and revise install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,11 @@
 
 INSTALL_DIR=~
 
-ln -si $PWD/zshrc           $INSTALL_DIR/.zshrc         && echo 'zshrc installed'
-ln -si $PWD/ctags           $INSTALL_DIR/.ctags         && echo 'ctags installed'
-ln -si $PWD/gitconfig       $INSTALL_DIR/.gitconfig     && echo 'gitconfig installed'
-ln -si $PWD/gdb             $INSTALL_DIR/.gdb           &&
-    ln -si $INSTALL_DIR/.gdb     $INSTALL_DIR/.gdbinit       && echo 'gdb installed'
+ln -sih $PWD/zshrc                 $INSTALL_DIR/.zshrc         && echo 'zshrc installed'
+ln -sih $PWD/ctags                 $INSTALL_DIR/.ctags         && echo 'ctags settings installed'
+ln -sih $PWD/gitconfig             $INSTALL_DIR/.gitconfig     && echo 'gitconfig installed'
+ln -sih $PWD/gdb                   $INSTALL_DIR/.gdb           && echo 'gdb setting installed'
+ln -sih $INSTALL_DIR/.gdb/gdbinit  $INSTALL_DIR/.gdbinit       && echo 'gdbinit installed'
+ln -sih $PWD/tmux.conf             $INSTALL_DIR/.tmux.conf     && echo 'tmux.conf installed'
 
 
