f7ecc0f68a39e18ceaf1a888e98fa79b57533abb
CrBoy <crboy@crboy.net>
2012-06-20 23:06:06 +0800

    Update install.sh for compatible with Linux and MacOSX

--- install.sh
+++ install.sh
@@ -2,11 +2,17 @@
 
 INSTALL_DIR=~
 
-ln -sih $PWD/zshrc                 $INSTALL_DIR/.zshrc         && echo 'zshrc installed'
-ln -sih $PWD/ctags                 $INSTALL_DIR/.ctags         && echo 'ctags settings installed'
-ln -sih $PWD/gitconfig             $INSTALL_DIR/.gitconfig     && echo 'gitconfig installed'
-ln -sih $PWD/gdb                   $INSTALL_DIR/.gdb           && echo 'gdb setting installed'
-ln -sih $INSTALL_DIR/.gdb/gdbinit  $INSTALL_DIR/.gdbinit       && echo 'gdbinit installed'
-ln -sih $PWD/tmux.conf             $INSTALL_DIR/.tmux.conf     && echo 'tmux.conf installed'
+if [ `uname` = "Linux" ]; then
+	LN='ln -sin'
+elif [ `uname` = "Darwin" ]; then
+	LN='ln -sih'
+fi
+
+${LN} $PWD/zshrc                 $INSTALL_DIR/.zshrc         && echo 'zshrc installed'
+${LN} $PWD/ctags                 $INSTALL_DIR/.ctags         && echo 'ctags settings installed'
+${LN} $PWD/gitconfig             $INSTALL_DIR/.gitconfig     && echo 'gitconfig installed'
+${LN} $PWD/gdb                   $INSTALL_DIR/.gdb           && echo 'gdb setting installed'
+${LN} $INSTALL_DIR/.gdb/gdbinit  $INSTALL_DIR/.gdbinit       && echo 'gdbinit installed'
+${LN} $PWD/tmux.conf             $INSTALL_DIR/.tmux.conf     && echo 'tmux.conf installed'
 
 
