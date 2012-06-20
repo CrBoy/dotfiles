e550a79633128e999033ca4f87db6a6fece9f34e
CrBoy <crboy@crboy.net>
2012-06-20 21:22:35 +0800

    Revise the install script

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 INSTALL_DIR=~
 
-ln -si $PWD/zshrc           $INSTALL_DIR/.zshrc
-ln -si $PWD/ctags           $INSTALL_DIR/.ctags
-ln -si $PWD/gitconfig       $INSTALL_DIR/.gitconfig
-ln -si $PWD/gdb             $INSTALL_DIR/.gdb
-ln -si $PWD/gdb/gdbinit     $INSTALL_DIR/.gdbinit
+ln -si $PWD/zshrc           $INSTALL_DIR/.zshrc         && echo 'zshrc installed'
+ln -si $PWD/ctags           $INSTALL_DIR/.ctags         && echo 'ctags installed'
+ln -si $PWD/gitconfig       $INSTALL_DIR/.gitconfig     && echo 'gitconfig installed'
+ln -si $PWD/gdb             $INSTALL_DIR/.gdb           &&
+    ln -si $INSTALL_DIR/.gdb     $INSTALL_DIR/.gdbinit       && echo 'gdb installed'
 
 
