c61a5ba3a036291b3b7ac8467cf517c69c8a8325
CrBoy <crboy@crboy.net>
2020-03-16 00:54:58 +0800

    Set LANG to en_US.UTF-8 in fish

--- stowable/fish/.config/fish/config.fish
+++ stowable/fish/.config/fish/config.fish
@@ -1,4 +1,5 @@
 set -g fish_user_paths "/usr/local/bin" "/usr/local/sbin" $fish_user_paths
+set -x LANG en_US.UTF-8
 
 status --is-interactive; and source (pyenv init -|psub)
 status --is-interactive; and source (pyenv virtualenv-init -|psub)
