6a4145ad379cdf26d0f1a4fba6cebe1a0f794a24
CrBoy <crboy@crboy.net>
2020-03-16 00:52:05 +0800

    Remove oh-my-zsh in setup script

--- install.sh
+++ install.sh
@@ -8,17 +8,6 @@ elif [ `uname` = "Darwin" ]; then
 	LN='ln -sih'
 fi
 
-hash curl >/dev/null 2>&1 && {
-	env curl -L http://install.ohmyz.sh | sh
-} || {
-	hash wget >/dev/null 2>&1 && {
-		env wget --no-check-certificate http://install.ohmyz.sh -O- | sh
-	} || {
-		echo "Cannot install oh-my-zsh"
-		exit
-	}
-}
-
 stow git && echo 'git stowed'
 stow vimrc && vimrc/install.sh && echo 'vimrc stowed'
 stow zshrc && echo 'zshrc stowed'
