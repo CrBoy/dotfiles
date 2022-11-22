af9c3617d83c92a1999f05f6b4adb260652006bb
CrBoy <crboy@crboy.net>
2022-11-22 18:29:40 +0800

    Update fish configs

--- stowable/fish/.config/fish/config.fish
+++ stowable/fish/.config/fish/config.fish
@@ -1,6 +1,6 @@
 set -g fish_user_paths "/usr/local/bin" "/usr/local/sbin" $fish_user_paths
 set -x LANG en_US.UTF-8
 
-status --is-interactive; and source (pyenv init -|psub)
-status --is-interactive; and source (pyenv virtualenv-init -|psub)
+# status --is-interactive; and source (pyenv init -|psub)
+# status --is-interactive; and source (pyenv virtualenv-init -|psub)
 
