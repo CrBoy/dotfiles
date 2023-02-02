27f3a09e97382142d12f63efb889779adce337f0
CrBoy <crboy@crboy.net>
2023-02-03 01:16:38 +0800

    Add homebrew (for Apple Silicon) path; Alias for adding all ssh key

--- stowable/fish/.config/fish/config.fish
+++ stowable/fish/.config/fish/config.fish
@@ -1,6 +1,9 @@
+eval (/opt/homebrew/bin/brew shellenv)
+
 set -g fish_user_paths "/usr/local/bin" "/usr/local/sbin" $fish_user_paths
 set -x LANG en_US.UTF-8
 
 # status --is-interactive; and source (pyenv init -|psub)
 # status --is-interactive; and source (pyenv virtualenv-init -|psub)
 
+alias ssh-add-all="find ~/.ssh/keys -type f ! -name '*.pub' | xargs ssh-add"
