7ced203a7ceb712999126e43f0ceceab093ecd81
CrBoy <crboy@crboy.net>
2024-06-05 01:40:05 +0800

    chore: save config

--- stowable/fish/dot-config/fish/config.fish
+++ stowable/fish/dot-config/fish/config.fish
@@ -1,6 +1,6 @@
 eval (/opt/homebrew/bin/brew shellenv)
 
-set -g fish_user_paths "/usr/local/bin" "/usr/local/sbin" $fish_user_paths
+set -g fish_user_paths "/usr/local/bin" "/usr/local/sbin" $fish_user_paths "/opt/homebrew/opt/python@3.12/libexec/bin"
 set -x LANG en_US.UTF-8
 
 # status --is-interactive; and source (pyenv init -|psub)
